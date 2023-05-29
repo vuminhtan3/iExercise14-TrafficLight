226c286b8539a1e9b86914f01b4f51a0976b23f3
Minh Tan Vu <vuminhtan.nuce3@gmail.com>
2023-05-29 20:54:47 +0700

    Complete Traffic Light with basic design

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var yellowButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redView.layer.borderWidth = 5
+        redView.layer.borderColor = UIColor.black.cgColor
+        redView.backgroundColor = .red
+        
     }
 
+    @IBAction func redButtonPressed(_ sender: UIButton) {
+        redView.backgroundColor = .red
+        yellowView.backgroundColor = .darkGray
+        greenView.backgroundColor = .darkGray
+    }
+    
+    @IBAction func yellowButtonPressed(_ sender: UIButton) {
+        redView.backgroundColor = .darkGray
+        yellowView.backgroundColor = .yellow
+        greenView.backgroundColor = .darkGray
+    }
 
+    @IBAction func greenButtonPressed(_ sender: UIButton) {
+        redView.backgroundColor = .darkGray
+        yellowView.backgroundColor = .darkGray
+        greenView.backgroundColor = .green
+    }
+    
+}
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        
+        set (value) {
+            layer.cornerRadius = value
+            layer.masksToBounds = true
+            self.clipsToBounds = true
+        }
+    }
 }
 
